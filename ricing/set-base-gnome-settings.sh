fc529450101dd0e5eaba022991ae67bf68cb8c2c
Harrison Held <harrydheld@gmail.com>
2026-06-24 20:32:40 -0500

    reorg

--- ricing/set-base-gnome-settings.sh
+++ ricing/set-base-gnome-settings.sh
@@ -6,7 +6,4 @@ gsettings set org.gnome.desktop.interface clock-show-weekday false
 gsettings set org.gnome.desktop.interface clock-show-date false
 gsettings set org.gnome.desktop.interface clock-show-seconds false
 
-# Disable Animations
-gsettings set org.gnome.desktop.interface enable-animations false
-
 echo "Base GNOME settings applied."
